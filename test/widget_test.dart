153039f9abe434483f32f0918a40d073b8e3440f
mohamedmagdy2301 <mohammedmego15@gmail.com>
2025-01-04 02:28:13 +0200

    Replace MyApp with UrlLauncherHelperUI in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:url_launcher_templete/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const UrlLauncherHelperUI());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
